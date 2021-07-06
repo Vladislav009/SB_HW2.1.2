18bca767f0bde2d59cad74f11528984b792f3d5e
Vladislav <developer@arthurandco.org>
2021-07-06 23:05:07 +0500

    Initial Commit

---
+++ SB_HW2.1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB_HW2.1.2
+//
+//  Created by Vladislav Kulak on 06.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
